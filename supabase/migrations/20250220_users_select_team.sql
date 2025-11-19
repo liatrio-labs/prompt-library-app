208d75a8ce874d7b1ec5628e6d077d3bde0bed9d
Josh Guice <josh.guice@liatrio.com>
2025-11-18 21:04:47 -0800

    chore(db): add team select policy for users (renamed migration)

--- supabase/migrations/20250220_users_select_team.sql
+++ supabase/migrations/20250220_users_select_team.sql
@@ -1,4 +1,4 @@
 -- Allow Liatrio teammates to read user records (to show author on prompts)
-create policy "Users: select team" on public.users
+create policy if not exists "Users: select team" on public.users
   for select
   using (email ilike '%@liatrio.com');
